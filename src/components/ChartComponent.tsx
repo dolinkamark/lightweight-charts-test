150f8494af768d04fffbb7363e7e974b72173df7
Dolinka Márk Gergely <dolinkamark@hotmail.com>
2025-08-15 21:11:04 +0200

    Initial commit

---
+++ src/components/ChartComponent.tsx
@@ -0,0 +1,68 @@
+import { AreaSeries, createChart, ColorType } from 'lightweight-charts';
+import { useEffect, useRef } from 'react';
+import { ActionPoint } from './ActionPoint';
+
+export const ChartComponent = props => {
+    const {
+        data,
+        colors: {
+            backgroundColor = '#0F0F0F',
+            lineColor = '#2962FF',
+            gridLineColor = '#2C2C2C',
+            textColor = '#9C9C9C',
+            areaTopColor = '#2962FF',
+            areaBottomColor = 'rgba(41, 98, 255, 0.28)',
+        } = {},
+    } = props;
+
+    const chartContainerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(
+        () => {
+            if (!chartContainerRef.current) return;
+
+            const handleResize = () => {
+                if (chartContainerRef.current) {
+                    chart.applyOptions({ width: chartContainerRef.current.clientWidth });
+                }
+            };
+
+            const chart = createChart(chartContainerRef.current, {
+                layout: {
+                    background: { type: ColorType.Solid, color: backgroundColor },
+                    textColor,
+                },
+                grid: {
+                    vertLines: {
+                        color: gridLineColor
+                    },
+                    horzLines: {
+                        color: gridLineColor
+                    }
+                },
+                width: chartContainerRef.current.clientWidth,
+                height: 300,
+            });
+            chart.timeScale().fitContent();
+
+            const newSeries = chart.addSeries(AreaSeries, { lineColor, topColor: areaTopColor, bottomColor: areaBottomColor });
+            newSeries.setData(data);
+
+            const actionPoint = new ActionPoint(chart, newSeries, { time: '2018-12-23', price: 32.51 });
+            newSeries.attachPrimitive(actionPoint);
+
+            window.addEventListener('resize', handleResize);
+
+            return () => {
+                window.removeEventListener('resize', handleResize);
+
+                chart.remove();
+            };
+        },
+        [data, backgroundColor, lineColor, textColor, areaTopColor, areaBottomColor]
+    );
+
+    return (
+        <div ref={chartContainerRef} />
+    );
+};
